90a9118941a23c6d73c53cdc2270f2ba416334db
Umer <uzahoor@mircomgroup.com>
2018-02-06 16:01:54 -0500

    updated documentation

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,14 +2,16 @@ import webbrowser
 import os
 import re
 
-
+# This is not my code this is taken from Udacity course for python 
+# with minor changes
 # Styles and scripting for the page
 main_page_head = '''
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <!-- Changed to my own title -->
+    <title>Umer's Favorite Movies!</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -64,21 +66,29 @@ main_page_head = '''
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
-        });
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', 
+            function (event) {
+                // Remove the src so the player itself gets removed, as this 
+                // is the only reliable way to ensure the video stops 
+                // playing in IE
+                $("#trailer-video-container").empty();
+            }
+        );
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
+            var sourceUrl = 
+            'http://www.youtube.com/embed/' +
+            trailerYouTubeId +
+            '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append(
+                $("<iframe></iframe>", {
+                'id': 'trailer-video',
+                'type': 'text-html',
+                'src': sourceUrl,
+                'frameborder': 0
+                }
+            ));
         });
         // Animate in the movies when the page loads
         $(document).ready(function () {
@@ -98,7 +108,8 @@ main_page_content = '''
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+          <a href="#" class="hanging-close" data-dismiss="modal"
+          aria-hidden="true">
             <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
@@ -122,7 +133,9 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center"
+ data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" 
+ data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
 </div>
